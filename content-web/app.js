6439f0f1f7de5e3355845676a001a6a751fe5eef
oap <irlab@irlab.ca>
2022-03-25 10:11:17 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("fe5ba61e-08a0-4418-9cb8-7c21beba4e47");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
